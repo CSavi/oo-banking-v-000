5a38f6e2394f97d7379d0ceeaae0c2fc22f8ebb3
Carrie Savinell <carrielane89@gmail.com>
2018-05-29 19:54:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end   
   end 
-  binding.pry 
+ # binding.pry 
   
   def reverse_transfer 
     if @status == "complete"
